fe117246e101ee7e1486b402f617de039de33eec
nahucroco <102126123+nahucroco@users.noreply.github.com>
2026-06-13 17:25:26 -0300

    Delete inmobiliaria directory

--- inmobiliaria/proyectoinmobiliaria/appinmobiliaria/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Propiedad
-
-# Register your models here.
-@admin.register(Propiedad)
-class PropiedadAdmin(admin.ModelAdmin):
-    list_display = ('id', 'ubicacion', 'imagen', 'precio', 'tamanio', 'descripcion') 
-    search_fields = ('ubicacion',)      
--- inmobiliaria/proyectoinmobiliaria/appinmobiliaria/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppinmobiliariaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appinmobiliaria'
